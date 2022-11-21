dc95fbc3f096635a4f9bb8e61580e405ba9ae99d
Wein <nikita0zrz@gmail.com>
2022-11-22 00:31:09 +0300

    Update policy.sql

--- policy.sql
+++ policy.sql
@@ -19,17 +19,31 @@ GRANT INSERT ON business.tasks, business.contact_info, business.contracts, busin
 GRANT UPDATE ON business.tasks, business.contact_info, business.contracts, business.person, business.organization
     TO manager;
 
-GRANT UPDATE(dt_finished) ON business.tasks TO worker;
+GRANT UPDATE(dt_finished, priority) ON business.tasks TO worker;
 
 ALTER TABLE business.tasks ENABLE ROW LEVEL SECURITY ;
 
 CREATE POLICY select_tasks ON business.tasks FOR SELECT TO manager, worker
 USING (
-    (SELECT username FROM business.user_login WHERE (user_login.id = author_id) = current_user OR
-    (SELECT username FROM business.user_login WHERE (user_login.id = executor_id) = current_user))
+    (SELECT username FROM business.user_login WHERE user_login.id = tasks.author_id) = current_user OR
+    (SELECT username FROM business.user_login WHERE user_login.id = tasks.executor_id) = current_user
     );
 
 CREATE POLICY select_for_managers ON business.tasks FOR SELECT TO manager
 USING (
-    ()
+    (SELECT title FROM business.roles WHERE roles.id = (SELECT role_id FROM business.person_role
+    WHERE person_id = tasks.author_id) = 'Сотрудник')
+    );
+
+CREATE POLICY access_all ON business.tasks FOR ALL TO administrator USING (true) WITH CHECK (true);
+
+CREATE POLICY insert_task ON business.tasks FOR INSERT TO manager WITH CHECK (true);
+
+CREATE POLICY update_task ON business.tasks FOR UPDATE TO manager USING (true) WITH CHECK(tasks.dt_finished IS NULL
+    AND ((SELECT username FROM business.user_login WHERE user_login.id = author_id) = current_user));
+
+CREATE POLICY update_state ON business.tasks FOR UPDATE TO manager, worker
+USING (dt_finished IS NULL) WITH CHECK((dt_finished IS NOT NULL)
+    AND ((SELECT username FROM business.user_login WHERE user_login.id = author_id) = current_user
+    OR (SELECT username FROM business.user_login WHERE user_login.id = executor_id) = current_user)
     );
